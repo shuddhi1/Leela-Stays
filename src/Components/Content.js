4d5e8bb96eeab5b706c0f6e63dc6121d00d3256d
Shuddhi1 <siddharthnaithani@hotmail.com>
2025-08-08 12:47:37 +0530

    chatbot

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,46 +1,36 @@
-import React from "react";
+import { Fab, Webchat } from '@botpress/webchat'
+import { useState } from 'react'
 
 function Content() {
+  const [isWebchatOpen, setIsWebchatOpen] = useState(false)
+  const toggleWebchat = () => {
+    setIsWebchatOpen((prevState) => !prevState)
+  }
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        padding: "40px 20px",
-        background: "#f9f9f9",
-        color: "#333",
-        fontFamily: "'Poppins', sans-serif",
-        lineHeight: "1.8",
-        maxWidth: "900px",
-        margin: "5px",
-        borderRadius: "12px",
-        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-        overflow: "auto",
-        flex:1
-      
-      }}
-    >
-      <section style={{ fontSize: "32px", fontWeight: "bold", marginBottom: "16px" }}>
-        Welcome to Leela Stays
-      </section>
-      <section style={{ fontSize: "24px", fontWeight: "600", marginBottom: "24px", color: "#2c7a7b" }}>
-        Your Cozy Retreat in the Hills
-      </section>
-      <section  style={{ textAlign: "justify", marginBottom: "24px", fontSize: "18px" }}>
-        Escape to the serene beauty of the hills with Leela Stays, where comfort meets nature.
-        Whether you're seeking a peaceful getaway, a romantic escape, or a family vacation, our
-        homestay offers an unforgettable experience surrounded by lush greenery and breathtaking
-        views. From peaceful nature trails to nearby cultural spots, Leela Stays is your gateway
-        to the treasures of the hills. Let us guide you to the best local experiences.
-      </section>
-      <section style={{ textAlign: "justify", fontSize: "18px" }}>
-        Ready to Unwind? Discover your home in the hills at Leela Stays. Book your stay today
-        and let the mountains work their magic!
-      </section>
-    </div>
-  );
+    <>
+      <Webchat
+        clientId="bf03e6cb-a6ac-43bc-af17-2977b83ffa36" // Your client ID here
+        style={{
+          width: '400px',
+          height: '600px',
+          display: isWebchatOpen ? 'flex' : 'none',
+          position: 'fixed',
+          bottom: '90px',
+          right: '20px',
+        }}
+      />
+      <Fab
+        onClick={() => toggleWebchat()}
+        style={{
+          position: 'fixed',
+          bottom: '20px',
+          right: '20px',
+          width: '64px',
+          height: '64px'
+        }}
+      />
+    </>
+  )
 }
 
 export default Content;
